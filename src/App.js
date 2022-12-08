a4cf7c80cec52eee2403ac5e7db9b2c35703fd18
eswar <eswarlesnar@gmail.com>
2022-12-08 16:35:44 +0530

    completed the chessboard knight functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,107 @@
-
+import boardArr from './data/boardArr';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+function App() { 
+  const [pending , setPending ] = useState(false)
+  const [selectedBlock , setSelectedBlock] = useState({})
+  const [moves , setMoves] = useState([])
+  const handleBlockClick =(e, item )=> {
+    e.stopPropagation()
+    if (pending === false) {
+      
+      setSelectedBlock(item);
+      document.getElementById(JSON.stringify(item)).classList.add('selected');
+      let j = 2;
+      let arr = [];
+
+      for (let i = 1; i < 3; i++) {
+     
+        arr.push({ row : item.row - i, col: item.col + j });
+        arr.push({ row : item.row - i, col: item.col - j });
+        arr.push({ row : item.row + i, col: item.col + j });
+        arr.push({ row : item.row + i, col: item.col - j });
+        setMoves(
+          arr.filter((val) => {
+            let inValidMove = true;
+            if (val.col < 1 || val.col > 8) {
+              inValidMove = false;
+            } else if (val.row < 1 || val.row > 8) {
+              inValidMove = false;
+            }
+            return inValidMove;
+          })
+        );
+        j--;
+      }
+      setPending(true);
+    } else {
+      setPending(false);
+      document
+        .getElementById(JSON.stringify(selectedBlock))
+        .classList.remove('selected');
+    }
+  };
+
+  useEffect(() => {
+    if (pending === true) {
+      if (!moves) {
+        setTimeout(() => {}, 500);
+      } else {
+        console.log(moves);
+        moves.forEach((x) => {
+          document.getElementById(JSON.stringify(x)).classList.add('moves');
+        });
+      }
+    } else {
+      if (!moves) {
+      } else {
+        moves.forEach((x) => {
+          document.getElementById(JSON.stringify(x)).classList.remove('moves');
+        });
+      }
+    }
+  }, [moves , pending]);
+  
   return (
     <div className="App">
-      
+       <header>
+
+       </header>
+       <div className='content'>
+         <div className='chessBoard'>
+           {boardArr.map(val => {
+             return <div className='row' key ={JSON.stringify(val)}>
+                 {
+                  val.map(item => {
+                     let blockcolor = ""
+                     if(item.row % 2 === 0 ){
+                        if(item.col % 2 ===0){
+                          blockcolor = "white"
+                        }else{
+                          blockcolor ="black"
+                        }
+                     }else{
+                      if(item.col % 2 ===0){
+                        blockcolor = "black"
+                      }else{
+                        blockcolor ="white"
+                      }
+                     }
+                     return <span 
+                          key ={JSON.stringify(item)} 
+                          id = {JSON.stringify(item)}
+                          className ={blockcolor}
+                          onClick ={e => {handleBlockClick(e, item)}}>
+
+                     </span>
+                  })
+                 }
+             </div>
+           })}
+         </div>
+         <div className='moves'></div>
+       </div>
     </div>
   );
 }

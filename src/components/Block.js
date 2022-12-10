534a089675bbdc5b7d1e08a5b6dd81dc097bfeaf
eswar <eswarlesnar@gmail.com>
2022-12-10 19:15:12 +0530

    added move indicators that show the row and col values

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,4 +1,6 @@
 const Block = ({item , blockcolor , handleBlockClick}) => {
+    let colObj  = {"1" : "a" , "2" :"b" , "3" : "c" , "4" : "d" , "5" : "e" , "6" :"f" ,"7" :"g" , "8" : "h"}
+    let rowObj = {"1" : "8" , "2" :"7" , "3" : "6" , "4" : "5" , "5" : "4" , "6" :"3" ,"7" :"2" , "8" : "1"}
 
     const getBorderRadius =() => {
         if(item.row ===1 && item.col === 1){
@@ -13,6 +15,27 @@ const Block = ({item , blockcolor , handleBlockClick}) => {
             return ""
         }
     }
-    return <span id ={JSON.stringify(item)} className={`${blockcolor} ${getBorderRadius()}`} onClick = {(e) => handleBlockClick(e , item) }></span>
+
+    const getColumnIden =() =>{
+        if(item.col === 1){
+            return rowObj[item.row]
+        }
+        return ""
+    }
+
+    const getRowIden = () =>{
+        if(item.row === 8) {
+            return colObj[item.col]
+        }
+        return ""
+    }
+    return <span 
+    id ={JSON.stringify(item)} 
+    className={`${blockcolor} ${getBorderRadius()}`} 
+    onClick = {(e) => handleBlockClick(e , item) }
+    style ={{position : "relative"}}>
+         <span style = {{position :"absolute" , top : "5px" , left :"8px" , color : "grey" , fontWeight :"400"}}>{getColumnIden()}</span>
+         <span style = {{position :"absolute" , bottom : "5px" , right :"8px" , color : "grey" , fontWeight :"400"}}>{getRowIden()}</span>
+    </span>
 }
 export default Block
